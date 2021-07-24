614555c3ed200f77455bb1233390f0b3700a5579
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-24 17:40:46 -0400

    added : Code to remove a contact from addressbook entries and table : .

--- js/address_book_form.js
+++ js/address_book_form.js
@@ -1,117 +1,125 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-    const name = document.querySelector('#name'); 
-    const textError = document.querySelector('.name-error');
-    name.addEventListener('input', function () {
-      let names = document.querySelector('#name').value.split(" ");
-      if(name.value.length == 0){
-        textError.textContent = "";
-        return;
-    }
-    try{
-        (new Contact()).firstName = names[0];
-        (new Contact()).lastName = names[1];
-        textError.textContent = "";
-    }catch(e){
-        textError.textContent = e;
-    }
-    });
-  
-    const addressElement = document.querySelector('#address');
-    const addressError = document.querySelector('.address-error');
-    addressElement.addEventListener('input', function() {
-      let address = document.querySelector('#address').value;
-      try{
-        (new Contact()).address = address;
-        addressError.textContent = ""
-      }catch(e){
-        addressError.textContent = e;
-      }
-    });
-  
-    const phoneElement = document.querySelector('#phone');
-    const phoneError = document.querySelector('.phone-error');
-    phoneElement.addEventListener('input', function() {
-      let phone = document.querySelector('#phone').value;
-      try{
-        (new Contact()).phone = phone;
-        phoneError.textContent = "";
-      }catch(e){
-        phoneError.textContent = e;
-      }
-  
-    });
-  
-    const emailElement = document.querySelector('#email');
-    const emailError = document.querySelector('.email-error');
-    emailElement.addEventListener('input', function() {
-      let email = document.querySelector('#email').value;
-      try{
-        (new Contact()).email = email;
-        emailError.textContent = "";
-      }catch(e){
-        emailError.textContent = e;
-      }
-  
-    });
+  const name = document.querySelector('#name'); 
+  const textError = document.querySelector('.name-error');
+  name.addEventListener('input', function () {
+    let names = document.querySelector('#name').value.split(" ");
+    if(name.value.length == 0){
+      textError.textContent = "";
+      return;
+  }
+  try{
+      (new Contact()).firstName = names[0];
+      (new Contact()).lastName = names[1];
+      textError.textContent = "";
+  }catch(e){
+      textError.textContent = e;
+  }
   });
-  
-  const save = (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    let contactData = createContact();
-    createAndUpdateStorage(contactData);
-  }
-  
-  const createContact = () => {
-    let contactData = new Contact();
-    let names = getInputValueById('#name').split(" ");
-    contactData.firstName = names[0];
-    contactData.lastName = names[1];
-    contactData.address = getInputValueById('#address');
-    contactData.city = getInputValueById('#city');
-    contactData.state = getInputValueById('#state');
-    contactData.zip = getInputValueById('#zip');
-    contactData.phone = getInputValueById('#phone');
-    contactData.email = getInputValueById('#email');
-    return contactData;
-  }
-  //storying data into local storage
-  function createAndUpdateStorage(contactData){
-    let contactList = JSON.parse(localStorage.getItem("ContactList"));
-  
-    if(contactList != undefined){
-        contactList.push(contactData);
+
+  const addressElement = document.querySelector('#address');
+  const addressError = document.querySelector('.address-error');
+  addressElement.addEventListener('input', function() {
+    let address = document.querySelector('#address').value;
+    try{
+      (new Contact()).address = address;
+      addressError.textContent = ""
+    }catch(e){
+      addressError.textContent = e;
     }
-    else{
-        contactList = [contactData];
+  });
+
+  const phoneElement = document.querySelector('#phone');
+  const phoneError = document.querySelector('.phone-error');
+  phoneElement.addEventListener('input', function() {
+    let phone = document.querySelector('#phone').value;
+    try{
+      (new Contact()).phone = phone;
+      phoneError.textContent = "";
+    }catch(e){
+      phoneError.textContent = e;
     }
-    alert(contactList.toString());
-    localStorage.setItem("ContactList",JSON.stringify(contactList));
+
+  });
+
+  const emailElement = document.querySelector('#email');
+  const emailError = document.querySelector('.email-error');
+  emailElement.addEventListener('input', function() {
+    let email = document.querySelector('#email').value;
+    try{
+      (new Contact()).email = email;
+      emailError.textContent = "";
+    }catch(e){
+      emailError.textContent = e;
+    }
+
+  });
+});
+
+const save = (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  let contactData = createContact();
+  createAndUpdateStorage(contactData);
+}
+
+const createContact = () => {
+  let contactList = JSON.parse(localStorage.getItem("ContactList"));
+  let max = 0;
+  if(contactList){
+      for(const contactData of contactList){
+          if(max<contactData._id)
+          max = contactData._id;
+      }
   }
-  
-  const getInputValueById = (id) => {
-    let value = document.querySelector(id).value;
-    return value;
+  let contactData = new Contact();
+  contactData.id = parseInt(max) + 1;
+  let names = getInputValueById('#name').split(" ");
+  contactData.firstName = names[0];
+  contactData.lastName = names[1];
+  contactData.address = getInputValueById('#address');
+  contactData.city = getInputValueById('#city');
+  contactData.state = getInputValueById('#state');
+  contactData.zip = getInputValueById('#zip');
+  contactData.phone = getInputValueById('#phone');
+  contactData.email = getInputValueById('#email');
+  return contactData;
+}
+
+function createAndUpdateStorage(contactData){
+  let contactList = JSON.parse(localStorage.getItem("ContactList"));
+
+  if(contactList != undefined){
+      contactList.push(contactData);
   }
-  
-  //setting retset form
-  const resetForm = () => {
-      setValue('#name','');
-      setValue('#address','');
-      setSelectedIndex('#city',0);
-      setSelectedIndex('#state',0);
-      setValue('#zip','');
-      setValue('#phone','');
-      setValue('#email','');
+  else{
+      contactList = [contactData];
   }
-  
-  const setValue = (id,value) => {
-    const element = document.querySelector(id);
-    element.value = value;
-  }
-  
-  const setSelectedIndex = (id,index) => {
-    const element = document.querySelector(id);
-    element.selectedIndex = index;
-  }
+  alert(contactList.toString());
+  localStorage.setItem("ContactList",JSON.stringify(contactList));
+}
+
+const getInputValueById = (id) => {
+  let value = document.querySelector(id).value;
+  return value;
+}
+
+const resetForm = () => {
+    setValue('#name','');
+    setValue('#address','');
+    setSelectedIndex('#city',0);
+    setSelectedIndex('#state',0);
+    setValue('#zip','');
+    setValue('#phone','');
+    setValue('#email','');
+}
+
+const setValue = (id,value) => {
+  const element = document.querySelector(id);
+  element.value = value;
+}
+
+const setSelectedIndex = (id,index) => {
+  const element = document.querySelector(id);
+  element.selectedIndex = index;
+}
